38ca4426198cbe719cc6950133a0bbcbd19ff917
~JVRC <140805892+JVRC03@users.noreply.github.com>
2025-09-17 16:58:37 +0530

    Create max_squares_within_given_baseOfTriangle.py

---
+++ Day_2/max_squares_within_given_baseOfTriangle.py
@@ -0,0 +1,10 @@
+n = int(input())
+
+k = n-2 #waste of 2 units all time
+
+print((k * (k+1))//2) 
+
+# - step wise being decreased everytime 
+# - ex : 4 -> 3 -> 2 -> 1
+# - until it reached 1
+# - SUM of 1st k natural numbers :: (n * (n+1)) // 2
